ca52d7f2ba2420222d17b7f0d201e906ed874e97
P1 <Marco80512>
2022-03-15 12:15:40 -0700

    css

--- Formwebapp.py
+++ Formwebapp.py
@@ -10,17 +10,15 @@ def render_main():
     
 @app.route("/Breakfast ")
 def render_response():
-    color = request.args['fname'] 
-    #The request object stores information about the request sent to the server.
-    #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
-    #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if color == 'pink':
-        reply1 = "That's my favorite color, too!"
-    else:
-        reply1 = "My favorite color is pink."
-    n = int(request.args['multNum']) #values in request.args are strings by default
-    reply2 = "2 x " + str(n) + " = " + str((2*n))
-    return render_template('PTJ.html', response1 = reply1, response2 = reply2)
+    c1 = request.args['choice1'] 
+    c2 = request.args['choice2'] 
+    c3 = request.args['choice3']
+    
+if c1 == 'Eggs' and c2 == 'French toast' and c3 == 'Syrup':
+    return render_tepmlate('PTJ.html) 
+    Meal = Markup('<img src="EFTS.jpg" alt="You have made French toast with Syrup" width="500" height="600">
+    <p> Where does this go?</p>
+    
     
 if __name__=="__main__":
     app.run(debug=True)
