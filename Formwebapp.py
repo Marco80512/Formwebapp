41159e69968a2e4c30943063cade01978a38b776
P1 <Marco80512>
2022-03-15 09:46:31 -0700

    css

--- Formwebapp.py
+++ Formwebapp.py
@@ -8,9 +8,9 @@ def render_main():
 
 
     
-@app.route("/Jobs")
+@app.route("/Breakfast ")
 def render_response():
-    color = request.args['Jobs'] 
+    color = request.args['fname'] 
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
