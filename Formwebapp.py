43ea45cca41fd05832713cf0d73c9e224b42400b
P1 <Marco80512>
2022-03-17 11:02:40 -0700

    css

--- Formwebapp.py
+++ Formwebapp.py
@@ -17,42 +17,96 @@ def render_response():
     c3 = request.args['choice3']
     
     if (c1 == 'Eggs' and c2 == 'French toast' and c3 == 'Syrup') or (c1 == 'Syrup' and c2 == 'Eggs' and c3 == 'French toast') or (c1 == 'French toast' and c2 == 'Syrup' and c3 == 'Eggs'):
-        return render_template('PTJ.html',Meal = Markup('<img src="/static/EFTS.jpg" alt="You have made French toast with Syrup" width="500" height="600"><p> You have made French toast with Syrup</p>'))
+        return render_template('PTJ.html',Meal = Markup('<img class="E1" src="/static/EFTS.jpg" alt="You have made French toast with Syrup" width="500" height="600"> class="E2"<p>You have made French toast with Syrup</p>'))
     
     
     if (c1 == 'Bananas' and c2 == 'Syrup' and c3 == 'Waffles') or (c1 == 'Waffles' and c2 == 'Bananas' and c3 == 'Syrup') or ( c1 == 'Syrup' and c2 == 'Waffles' and c3 == 'Bananas'):
-        return render_template('PTJ.html',Meal = Markup('<img src="/static/BSW.jpg" alt="You have made Waffle with Syrup" width="500" height="600"><p> You have made Waffle with Syrup</p>'))  
+        return render_template('PTJ.html',Meal = Markup('<img class="E1" src="/static/BSW.jpg" alt="You have made Waffle with Syrup" width="500" height="600"><p> You have made Waffle with Syrup</p>'))  
         
         
     if (c1 == 'Bananas' and c2 == 'Yogurt' and c3 == 'Oatmeal') or (c1 == 'Oatmeal' and c2 == 'Bananas' and c3 == 'Yogurt') or (c1 == 'Yogurt' and c2 == 'Oatmeal' and c3 == 'Bananas'):
-        return render_template('PTJ.html',Meal = Markup('<img src="/static/YOB.jpg" alt="You have made Oatmeal with Bananas " width="500" height="600"><p> You have made Oatmeal with Bananas</p>'))   
+        return render_template('PTJ.html',Meal = Markup('<img class="E1" src="/static/YOB.jpg" alt="You have made Oatmeal with Bananas " width="500" height="600"><p> You have made Oatmeal with Bananas</p>'))   
         
  
     if (c1 == 'Bananas' and c2 == 'Syrup' and c3 == 'Pancakes') or (c1 == 'Pancakes' and c2 == 'Bananas' and c3 == 'Syrup') or (c1 == 'Syrup' and c2 == 'Pancakes' and c3 == 'Bananas'):
-        return render_template('PTJ.html',Meal = Markup('<img src="/static/BSP.jpg" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You have made Pancakes with Bananas</p>'))
+        return render_template('PTJ.html',Meal = Markup('<img class="E1" src="/static/BSP.jpg" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You have made Pancakes with Bananas</p>'))
         
   
     if (c1 == 'Pancakes' and c2 == 'Bananas' and c3 == 'Syrup') or (c1 == 'Syrup' and c2 == 'Pancakes' and c3 == 'Bananas') or (c1 == 'Bananas' and c2 == 'Syrup' and c3 == 'Pancakes'):
-        return render_template('PTJ.html',Meal = Markup('<img src="/static/MEB.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You have made Banana Cake</p>'))
+        return render_template('PTJ.html',Meal = Markup('<img class="E1" src="/static/MEB.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You have made Banana Cake</p>'))
         
       
     if (c1 == 'Milk' and c2 == 'Cheese' and c3 == 'Syrup') or (c1 == 'Syrup' and c2 == 'Milk' and c3 == 'Cheese') or (c1 == 'Cheese' and c2 == 'Syrup' and c3 == 'Milk'):
-        return render_template('PTJ.html',Meal = Markup('<img src="/static/SF.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You can not make anything with these items</p>'))
+        return render_template('PTJ.html',Meal = Markup('<img class="E1" src="/static/SF.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You can not make anything with these items</p>'))
         
         
     if (c1 == 'Eggs' and c2 == 'Yogurt' and c3 == 'Milk') or ( c1 == 'Milk' and c2 == 'Eggs' and c3 == 'Yogurt') or (c1 == 'Yogurt' and c2 == 'Milk' and c3 == 'Eggs'):
-        return render_template('PTJ.html',Meal = Markup('<img src="/static/SF.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You can not make anything with these items</p>'))
+        return render_template('PTJ.html',Meal = Markup('<img class="E1" src="/static/SF.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You can not make anything with these items</p>'))
         
         
     if (c1 == 'Eggs' and c2 == 'Yogurt' and c3 == 'Syrup') or ( c1 == 'Syrup' and c2 == 'Eggs' and c3 == 'Yogurt') or (c1 == 'Yogurt' and c2 == 'Syrup' and c3 == 'Eggs'):
-        return render_template('PTJ.html',Meal = Markup('<img src="/static/SF.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You can not make anything with these items</p>'))
+        return render_template('PTJ.html',Meal = Markup('<img class="E1" src="/static/SF.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You can not make anything with these items</p>'))
         
         
     if (c1 == 'Eggs' and c2 == 'Pancakes' and c3 == 'Waffles') or ( c1 == 'Waffles' and c2 == 'Eggs' and c3 == 'Pancakes') or (c1 == 'Pancakes' and c2 == 'Waffles' and c3 == 'Eggs'):
-        return render_template('PTJ.html',Meal = Markup('<img src="/static/WPE.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You have made Waffles with Eggs and Pancakes</p>'))
+        return render_template('PTJ.html',Meal = Markup('<img class="E1" src="/static/WPE.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You have made Waffles with Eggs and Pancakes</p>'))
+        
         
     if (c1 == 'French toast' and c2 == 'Syrup' and c3 == 'Pancakes') or ( c1 == 'Pancakes' and c2 == 'French toast' and c3 == 'Syrup') or (c1 == 'Syrup' and c2 == 'Pancakes' and c3 == 'French toast'):
-        return render_template('PTJ.html',Meal = Markup('<img src="/static/FTPS.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You can not make anything with these items</p>'))
+        return render_template('PTJ.html',Meal = Markup('<img class="E1" src="/static/SF.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You can not make anything with these items</p>'))
+    
+    
+    if (c1 == 'Oatmeal' and c2 == 'Waffles' and c3 == 'Pancakes') or ( c1 == 'Pancakes' and c2 == 'Oatmeal' and c3 == 'Waffles') or (c1 == 'Syrup' and c2 == 'Pancakes' and c3 == 'French toast'):
+        return render_template('PTJ.html',Meal = Markup('<img class="E1" src="/static/SF.png" alt="You have made Pancakes with Bananas " width="500" height="600">  <p> You can not make anything with these items</p>'))
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
     
